41c4b4735b28e32ececb182eee43ea5af4498767
Thanseeh yaseen <yaseenkdr27@gmail.com>
2023-06-29 23:55:36 +0530

    inserting documents in mongodb

--- mongo_basics.js
+++ mongo_basics.js
@@ -7,3 +7,11 @@ db.users.find({ age: 25 })  //basic find query
 db.users.find({ age: 25 }, { name: 1, _id: 0 }) //projection query
 
 db.users.find({ age: { $gt: 30 } }) //comparison query
+
+// Inserting Documents:
+db.users.insertOne({ name: "John Doe", age: 30, email: "john@example.com" });
+
+db.users.insertMany([
+  { name: "Jane Smith", age: 35, email: "jane@example.com" },
+  { name: "Mark Johnson", age: 28, email: "mark@example.com" },
+]);
